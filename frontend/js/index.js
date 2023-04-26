0192eee0309c60b0065d7bcb0249b80f1f822bc1
NafiRanta <nafisah.rantasalmi@gmail.com>
2023-04-26 18:57:40 +0300

    added fake json data

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,4 @@
 
-
 document.querySelector("login-page").style.display = "block";
 document.querySelector("dashboard-page").style.display = "none";
 
@@ -11,23 +10,3 @@ document.getElementById("loginSubmit").addEventListener("click", login);
 
 
 
-async function login(){
-    const credentials = `${username}:${password}`;
-    const encodedCredentials = new TextEncoder().encode(credentials);
-    const base64Credentials = fromByteArray(encodedCredentials);
-    const response = await fetch('http://localhost:8080/api/auth/signin', {
-    method: 'POST',
-    headers: {
-        'Authorization': `Basic ${base64Credentials}`,
-    },
-    });
-
-    if (!response.ok) {
-        const error = await response.text();
-        throw new Error(error);
-    }
-
-    const data = await response.json();
-    console.log("data: ", data);
-    return data.token;
-}
