b5d6bbd70becce94beb478ceb36d44bd54834288
NafiRanta <nafisah.rantasalmi@gmail.com>
2023-05-17 16:08:50 +0300

    update

--- js/router.js
+++ js/router.js
@@ -3,19 +3,24 @@ const urlRoutes = {
     '/graphql/': {
         template: "<dashboard-page></dashboard-page>"
     },
+    '/': {
+        template: "<dashboard-page></dashboard-page>"
+    },
     '/login': {
         template: "<login-page></login-page>"
-    },
+    }
 };
 
 const urlLocationHolder = async () => {
     const path = window.location.pathname;
     const jwt = localStorage.getItem('jwt');
     let location = path;
-    if (jwt == null && path === '/graphql/') {
+    console.log("location", location)
+    if (jwt == null && (path === '/graphql/' || path == '/')) {
         location = '/login';
     }
     const route = urlRoutes[location];
+    console.log("route", route)
     document.getElementById('main').innerHTML = route.template;
 
 };
